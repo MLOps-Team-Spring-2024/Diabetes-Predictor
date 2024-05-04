a30f829f8fd81db00a819f210428777fd700990d
Matt Soria <matt@Matts-MacBook-Pro.local>
2024-05-03 22:53:57 -0500

    fix mypy issues

--- mlops_team_project/models/xgboost_model.py
+++ mlops_team_project/models/xgboost_model.py
@@ -1,10 +1,15 @@
-import hydra
-import omegaconf
-import pandas as pd
-from omegaconf import OmegaConf
+from typing import List
 
-from mlops_team_project.src.model.model import model
-from mlops_team_project.src.preprocess.preprocess import (
+import hydra
+import numpy as np
+import omegaconf
+from omegaconf import OmegaConf
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+import xgboost as xgb
+
+from mlops_team_project.src.preprocess import (
     min_max_scale_and_write,
     train_test_split_and_write,
 )
@@ -38,6 +43,42 @@ def main(config) -> None:
         y_test=y_test,
         hyperparameters=hydra_params,
     )
+
+
+def model(
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    y_train: np.ndarray,
+    y_test: np.ndarray,
+    hyperparameters: omegaconf.dictconfig.DictConfig,
+    target_names: List[str] = ["non-diabetic", "diabetic"],
+) -> None:
+    """
+    Runs the XGBoost model.
+
+    Args:
+        X_train: train dataset.
+        X_test: test dataset.
+        y_train: labels for training.
+        y_test: labels for test.
+    """
+    model = xgb.XGBClassifier(
+        use_label_encoder=False,
+        random_state=hyperparameters.seed,
+        n_estimators=hyperparameters.n_estimators,
+    )
+
+    cv_scores = cross_val_score(model, X_train, y_train, cv=5)
+
+    model.fit(X_train, y_train)
+    base_model_preds = model.predict(X_test)
+
+    print(f"cv scores = {cv_scores}")
+    print(f"cv scores avg = {cv_scores.mean()}")
+    print(
+        f"Training: {model.score(X_train, y_train)}, Testing: {model.score(X_test, y_test)}\n"
+    )
+    print(classification_report(y_test, base_model_preds, target_names=target_names))
 
 
 if __name__ == "__main__":
