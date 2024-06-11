8dc463ded2b5d0f796aa19df5280fb9a1e8c0f97
Henry deBuchananne <henrycd95@hotmail.com>
2024-06-10 23:20:02 -0500

    remove unnecessary tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 from hydra import compose, initialize
 from omegaconf import OmegaConf
+from xgboost import XGBClassifier
 
 from mlops_team_project.models.xgboost_model import model
 from tests import _HYDRA_CONFIG
@@ -18,8 +19,10 @@ def input_data():
     target_names = ["non-diabetic", "diabetic"]
     return X_train, X_test, y_train, y_test, target_names
 
-
 def test_model(input_data):
+    """
+        Unit test for model size
+    """
     X_train, X_test, y_train, y_test, target_names = input_data
 
     with patch('mlops_team_project.models.xgboost_model.xgb.XGBClassifier') as xgb_model, \
@@ -34,7 +37,6 @@ def test_model(input_data):
         xgb_model.return_value = mock_model
         cv_score.return_value = np.array([1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7])
         cv_score.mean.return_value = 1.1
-        logging.side_effect = lambda *args, **kwargs: None
         hydra_config = OmegaConf.load(_HYDRA_CONFIG)
 
         response = model(X_train, X_test, y_train, y_test, hydra_config, target_names)
